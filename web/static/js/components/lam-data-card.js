24381ed5f328ccc9acfb34b4f30cabd2e82c4373
tsimoli <turo.simolin@gmail.com>
2016-06-07 14:09:17 +0300

    Lamdata card modifications

--- web/static/js/components/lam-data-card.js
+++ web/static/js/components/lam-data-card.js
@@ -9,17 +9,19 @@ export default class LamDataCard extends React.Component {
     const { lamData }  = this.props;
     var measurement_time = new Date(lamData.measurement_time);
     var dateOptions = {
-      weekday: "long", year: "numeric", month: "short",
+      year: "numeric", month: "numeric",
       day: "numeric", hour: "2-digit", minute: "2-digit"
     }
     return (
-          <div className="col s3">
-            <div className="card green darken-1">
-              <div className="card-content white-text">
+          <div className="col s2">
+            <div className="card light-green accent-2">
+              <div className="card-content black-text">
                 <span className="card-title">{ lamData.location_name }</span>
                 <p>{ lamData.average_speed2 } km/h</p>
                 <p>{ lamData.traffic_volume2 } autoa/h</p>
-                <p>{ measurement_time.toLocaleDateString("fi-FI", dateOptions) }</p>
+                <div class="card-action">
+                  <p>{ measurement_time.toLocaleDateString("fi-FI", dateOptions) }</p>
+                </div>
               </div>
             </div>
           </div>
